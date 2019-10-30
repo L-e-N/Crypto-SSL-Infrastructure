c4305583aecf2114158f7149a46ac82226005eb6
L-e-N <leo.ng@hotmail.fr>
2019-10-30 17:18:41 +0100

    Changes to Equipement.py to begin certifiate chaining and DA synchronization

--- Equipement.py
+++ Equipement.py
@@ -12,7 +12,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 
 from Certificat import Certificat
 from create_socket import *
-
+from itertools import chain
 
 class Equipment:
 
@@ -69,5 +69,53 @@ class Equipment:
         else:
             self.ca[certifying_equipment_name] = {certified_equipment_name: (cert, certifying_equipment_pub_key)}
 
+    def synchronize_da(self, equipment):
+        # add the (keys,values) from equipment.da and equipment.ca that are NOT in self.da
+        self.da = dict(dict(equipment.da, **equipment.ca), **self.da)
+        for sda in self.da:
+            # for each key of self.DA, add all the (key,value) of equipment.CA[key] and DA[key] that are not in self.DA[key]
+            self.da[sda] = dict(dict(equipment.da.get(sda, {}), **equipment.ca.get(sda, {})), **self.da[sda])
+
+    # is there a path from the current equipment to another equipment ?
+    def chain_exists(self, equipment):
+        # pick a dictionary which contains the end node
+        dic = {}
+        for eda, v in chain(equipment.da):
+            if v.get(self.name, False):
+                dic = v
+                break
+        if dic == {}:
+            return False  # the chain does not exist
+        else:
+            findchain(self.name, equipment.name, v)
 
 
+def findchain(start, end, graph):
+    return ""
+
+
+# tests for dictionary unions
+"""  a -- b -- c """
+CAa = {'b': {'a': ('certba', 'pubb')}}
+CAb = {'a': {'b': ('certab', 'puba')}, 'c': {'b': ('certcb', 'pubc')}}
+CAc = {'b': {'c': ('certbc', 'pubc')}}
+DAa = {'c': {'b': ('certcb', 'pubc')}}
+DAb = {}
+DAc = {'c': {'b': ('certcbprime', 'pubc')}}
+# NOTE : dict(CAa, **DAc) union only with the keys. Does not go in depth. If key conflict, DAc values are used
+# Same for CAa.update(DAc), which updates the CAa values with the DAc values
+
+
+
+# DAafter = defaultdict(list)
+# for k, v in chain(CAa.items(), DAa.items()):
+#    DAafter[k].append(v)
+
+
+def synchrodict(DAa, DAc, CAc):
+    DAabis = dict(dict(DAc, **CAc), **DAa)
+
+    for sda in DAa:
+        # for each key of self.DA, add all the keys of equipment.CA and DA that are not in self.DA
+        DAabis[sda] = dict(dict(DAc.get(sda, {}), **CAc.get(sda, {})), **DAa[sda])
+    return DAabis
